0b0a86e0e171dab4c8618dd6627a5a3d66989e97
Himanshu <72097380+heyhimansh@users.noreply.github.com>
2022-09-20 23:34:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-/**
- * Guess The Number Game
- * TODO: Get user value from input and save it to variable numberGuess
- * TODO: Generate a random number 1 to 100 and save it to variable correctNumber
- * TODO: Console whether the guess is too high, too low, or is correct inside playGame function
- * TODO: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
- * TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
- * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
- * TODO: Save the guess history in a variable called guess
- * TODO: Display the guess history using displayHistory() function
- * TODO: Use the initGame() function to restart the game
- */
 
 // Variable to store the list of guesses 
 
